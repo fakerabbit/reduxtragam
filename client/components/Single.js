97079e6806dcae7ec5152fc1b31fd799504e6efe
Mirko <mirkoj@gmail.com>
2017-07-24 22:00:37 -0500

    Fixed Link index

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
   render() {
+    const params = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === params.postId);
+    console.log(i);
     return (
       <div className="single-photo">
         I'm the Single
